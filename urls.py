72a9d6fdeabb83dd53bf88a6217ffa2043baee35
Stolbik <110119436+Stolbik@users.noreply.github.com>
2022-08-29 01:16:22 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,7 +6,4 @@ from glpage.views import catAPI
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('glpage.urls')),
-    path('api/v1/questions/', Createtgq.as_view()),
-    path('api/v1/getcategories/', catAPI.as_view()),
-    path('api/v1/questions/<str:username>', Createtgq.as_view())
 ]
